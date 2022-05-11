3d7c60b30b4c3499459c1960f643d5e829dee106
Gage Clancy <gclancy121@gmail.com>
2022-05-11 16:09:16 -0500

    finished

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,9 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+
+  return ([darkMode, setDarkMode]);
+};
+
+export default useDarkMode;
